252fe1c6391457a8c9999a0ee312404656381458
Dosenpfand <m@sad.bz>
2023-05-08 19:56:35 +0200

    Add zip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 use clap::Parser;
 
 /// TODO
@@ -9,8 +11,19 @@ struct Cli {
 
 fn main() {
     let args = Cli::parse();
+    let file = fs::File::open(args.in_file).unwrap();
 
-    println!("{0}", args.in_file.display());
+    let mut archive = zip::ZipArchive::new(file).unwrap();
 
-    println!("Hello, world!");
+    for i in 0..archive.len() {
+        let mut file = archive.by_index(i).unwrap();
+        let out_path = match file.enclosed_name() {
+            Some(path) => path.to_owned(),
+            None => continue,
+        };
+
+        if (*file.name()).ends_with('/') {
+            println!("{}: Found folder \"{}\"", i, out_path.display());
+        }
+    }
 }
