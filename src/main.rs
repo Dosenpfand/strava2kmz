33fd6674a4df22cd1a38cc38bbc81a2a16aea09c
Dosenpfand <m@sad.bz>
2023-05-08 20:51:19 +0200

    Add some serde

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::fs;
 
 use clap::Parser;
 
+use serde::Deserialize;
+
 /// TODO
 #[derive(Parser)]
 struct Cli {
@@ -9,21 +11,25 @@ struct Cli {
     in_file: std::path::PathBuf,
 }
 
+#[derive(Debug, Deserialize)]
+struct InRecord {
+    #[serde(rename(deserialize = "Activity ID"))]
+    activity_id: String,
+    #[serde(rename(deserialize = "Media"))]
+    photos: String,
+}
+
 fn main() {
     let args = Cli::parse();
     let file = fs::File::open(args.in_file).unwrap();
 
     let mut archive = zip::ZipArchive::new(file).unwrap();
+    let mut activities_file = archive.by_name("activities.csv").unwrap();
+    let mut rdr = csv::Reader::from_reader(activities_file);
 
-    for i in 0..archive.len() {
-        let mut file = archive.by_index(i).unwrap();
-        let out_path = match file.enclosed_name() {
-            Some(path) => path.to_owned(),
-            None => continue,
-        };
+    for result in rdr.deserialize() {
+        let record: InRecord = result.unwrap();
 
-        if (*file.name()).ends_with('/') {
-            println!("{}: Found folder \"{}\"", i, out_path.display());
-        }
+        println!("{:?}", record);
     }
 }
