6122f5eb85f90b3b6161ca963fd563f0314b8096
Dosenpfand <m@sad.bz>
2023-05-08 22:32:08 +0200

    use gpx to kml converte

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use std::{fs, result};
-
 use clap::Parser;
-
+use gpx_kml_convert::convert;
 use serde::{Deserialize, Deserializer};
+use std::{
+    fs,
+    io::{stdout, Read},
+    result,
+};
 
 /// TODO
 #[derive(Parser)]
@@ -16,11 +19,16 @@ struct InRecord {
     // TODO: not possible to have string slices?
     #[serde(rename(deserialize = "Activity ID"))]
     activity_id: String,
-    #[serde(rename(deserialize = "Media"), deserialize_with = "InRecord::deserialize_media")]
+    #[serde(rename(deserialize = "Filename"))]
+    filename: String,
+    #[serde(
+        rename(deserialize = "Media"),
+        deserialize_with = "InRecord::deserialize_media"
+    )]
     media: Vec<String>,
 }
 
-impl InRecord  {
+impl InRecord {
     fn deserialize_media<'de, D>(deserializer: D) -> result::Result<Vec<String>, D::Error>
     where
         D: Deserializer<'de>,
@@ -36,12 +44,19 @@ fn main() {
     let file = fs::File::open(args.in_file).unwrap();
 
     let mut archive = zip::ZipArchive::new(file).unwrap();
-    let activities_file = archive.by_name("activities.csv").unwrap();
-    let mut rdr = csv::Reader::from_reader(activities_file);
 
-    for result in rdr.deserialize() {
-        let mut record: InRecord = result.unwrap();
+    let mut records: Vec<InRecord> = vec![];
+    {
+        let activities_file = archive.by_name("activities.csv").unwrap();
+        let mut rdr = csv::Reader::from_reader(activities_file);
 
-        println!("{:?}", &record);
+        records = rdr.deserialize().map(|x| x.unwrap()).collect();
+
+        println!("{:?}", records);
+    }
+    {
+        let mut record_file = archive.by_name(&records[0].filename).unwrap();
+        let mut sink = stdout();
+        convert(&mut record_file, &mut sink).unwrap();
     }
 }
