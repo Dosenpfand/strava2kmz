7e7df268ffe6982676165eec15210155fe6ff006
Dosenpfand <m@sad.bz>
2023-05-09 13:18:49 +0200

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@ use clap::Parser;
 use gpx_kml_convert::convert;
 use serde::{Deserialize, Deserializer};
 use std::{
-    fs,
+    fs::{self, File},
     io::{stdout, Read},
     result,
 };
+use zip::{write::FileOptions, ZipArchive};
 
-/// TODO
+/// Convert a strave export archive to a set of kmz files.
 #[derive(Parser)]
 struct Cli {
     /// The path to the file to read
@@ -15,28 +16,55 @@ struct Cli {
 }
 
 #[derive(Debug, Deserialize)]
-struct InRecord {
-    // TODO: not possible to have string slices?
+struct Record {
+    // TODO: string slices?
     #[serde(rename(deserialize = "Activity ID"))]
     activity_id: String,
     #[serde(rename(deserialize = "Filename"))]
     filename: String,
     #[serde(
         rename(deserialize = "Media"),
-        deserialize_with = "InRecord::deserialize_media"
+        deserialize_with = "Record::deserialize_media"
     )]
     media: Vec<String>,
 }
 
-impl InRecord {
+impl Record {
     fn deserialize_media<'de, D>(deserializer: D) -> result::Result<Vec<String>, D::Error>
     where
         D: Deserializer<'de>,
         &'de str: Deserialize<'de>,
     {
         let s: &str = Deserialize::deserialize(deserializer)?;
-        Ok(s.split("|").map(|s| s.to_string()).collect())
+        Ok(s.split('|')
+            .filter(|&x| !x.is_empty())
+            .map(|s: &str| s.to_string())
+            .collect())
     }
+}
+
+fn extract_records(zip_file: &mut zip::ZipArchive<File>) -> Vec<Record> {
+    let activities_file = zip_file.by_name("activities.csv").unwrap();
+    let mut rdr = csv::Reader::from_reader(activities_file);
+    rdr.deserialize().map(|x| x.unwrap()).collect()
+}
+
+fn write_kmz(zip_file: &mut zip::ZipArchive<File>, record: &Record) {
+    let mut record_file: zip::read::ZipFile = zip_file.by_name(&record.filename).unwrap();
+    let out_file_name = format!("{}.kmz", record.activity_id);
+    let path = std::path::Path::new(&out_file_name);
+    let file = std::fs::File::create(path).unwrap();
+    let mut zip_writer = zip::ZipWriter::new(file);
+    let options = FileOptions::default()
+        .compression_method(zip::CompressionMethod::Stored)
+        .unix_permissions(0o755);
+    zip_writer.start_file("doc.kml", options).unwrap();
+
+    convert(&mut record_file, &mut zip_writer).unwrap();
+
+    println!("{:?}", record);
+
+    zip_writer.finish().unwrap();
 }
 
 fn main() {
@@ -45,18 +73,9 @@ fn main() {
 
     let mut archive = zip::ZipArchive::new(file).unwrap();
 
-    let mut records: Vec<InRecord> = vec![];
-    {
-        let activities_file = archive.by_name("activities.csv").unwrap();
-        let mut rdr = csv::Reader::from_reader(activities_file);
-
-        records = rdr.deserialize().map(|x| x.unwrap()).collect();
-
-        println!("{:?}", records);
-    }
-    {
-        let mut record_file = archive.by_name(&records[0].filename).unwrap();
-        let mut sink = stdout();
-        convert(&mut record_file, &mut sink).unwrap();
-    }
+    let records = extract_records(&mut archive);
+    records
+        .into_iter()
+        .map(|x| write_kmz(&mut archive, &x))
+        .for_each(drop);
 }
