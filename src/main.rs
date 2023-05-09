b16167cc9b3cb9f32e5a180c1c6449025a80b4eb
Dosenpfand <m@sad.bz>
2023-05-09 14:15:30 +0200

    Write media files to kmz

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use clap::Parser;
+use flate2::read::GzDecoder;
 use gpx_kml_convert::convert;
 use serde::{Deserialize, Deserializer};
 use std::{
     fs::{self, File},
-    io::{stdout, Read},
+    io::{stdout, Read, Write, copy},
     result,
 };
 use zip::{write::FileOptions, ZipArchive};
-use flate2::read::GzDecoder;
 
 /// Convert a strave export archive to a set of kmz files.
 #[derive(Parser)]
@@ -27,7 +27,7 @@ struct Record {
         rename(deserialize = "Media"),
         deserialize_with = "Record::deserialize_media"
     )]
-    media: Vec<String>,
+    medias: Vec<String>,
 }
 
 impl Record {
@@ -51,28 +51,37 @@ fn extract_records(zip_file: &mut zip::ZipArchive<File>) -> Vec<Record> {
 }
 
 fn write_kmz(zip_file: &mut zip::ZipArchive<File>, record: &Record) {
-    let mut record_file = zip_file.by_name(&record.filename).unwrap();
-    
-    let out_file_name = format!("{}.kmz", record.activity_id);
-    let path = std::path::Path::new(&out_file_name);
-    let file = std::fs::File::create(path).unwrap();
-    let mut zip_writer = zip::ZipWriter::new(file);
-    let options = FileOptions::default()
+    let kmz_file_name = format!("{}.kmz", record.activity_id);
+    let kmz_path = std::path::Path::new(&kmz_file_name);
+    let kmz_file = std::fs::File::create(kmz_path).unwrap();
+    let mut kmz_writer = zip::ZipWriter::new(kmz_file);
+    let zip_options = FileOptions::default()
         .compression_method(zip::CompressionMethod::Stored)
         .unix_permissions(0o755);
-    zip_writer.start_file("doc.kml", options).unwrap();
 
-    if record.filename.ends_with(".gz") {
-        let mut decoder = GzDecoder::new(record_file);
-        convert(&mut decoder, &mut zip_writer).unwrap();
+    {
+        let mut track_file: zip::read::ZipFile = zip_file.by_name(&record.filename).unwrap();
+        kmz_writer.start_file("doc.kml", zip_options).unwrap();
+
+        if record.filename.ends_with(".gz") {
+            let mut gz_decoder = GzDecoder::new(track_file);
+            convert(&mut gz_decoder, &mut kmz_writer).unwrap();
+        } else {
+            convert(&mut track_file, &mut kmz_writer).unwrap();
+        }
     }
-    else {
-        convert(&mut record_file, &mut zip_writer).unwrap();
+
+    {
+        for media_file_name in &record.medias {
+            kmz_writer.start_file(media_file_name, zip_options).unwrap();
+            let mut media_file = zip_file.by_name(&media_file_name).unwrap();
+            copy(&mut media_file, &mut kmz_writer).unwrap();
+        }
     }
+
+    kmz_writer.finish().unwrap();
 
     println!("{:?}", record);
-
-    zip_writer.finish().unwrap();
 }
 
 fn main() {
