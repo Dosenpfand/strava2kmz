2a19d8b264dc2bb4a74847116c5e99e7e7bcc274
Dosenpfand <m@sad.bz>
2023-05-10 15:51:21 +0200

    Add output dir parameter

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
 use anyhow::{Context, Result};
 use clap::Parser;
-use std::fs;
+use std::{fs, path::PathBuf};
 use strava2kmz::{extract_records, Kmz, Record};
 
 /// Convert a strave export archive to a set of kmz files.
 #[derive(Parser)]
 struct Cli {
     /// The path to the file to read
-    in_file: std::path::PathBuf,
+    in_file: PathBuf,
+    /// The directory where the output is written
+    out_dir: Option<PathBuf>,
 }
 
 fn main() -> Result<()> {
     let args = Cli::parse();
     let in_file = &args.in_file;
+    let out_dir = &args.out_dir.unwrap_or_else(PathBuf::new);
+
     let file =
         fs::File::open(in_file).with_context(|| format!("Could not open {}", in_file.display()))?;
 
@@ -24,7 +28,11 @@ fn main() -> Result<()> {
 
     records
         .into_iter()
-        .map(|x: Record| Kmz::convert(&format!("{}.kmz", &x.activity_id()), &mut archive, &x))
-        .for_each(drop);
-    Ok(())
+        .try_for_each(|x: Record| {
+            let mut out_path = out_dir.clone();
+            out_path.push(x.activity_id());
+            out_path.set_extension("kmz");
+            Kmz::convert(&out_path.to_string_lossy(), &mut archive, &x)
+        })
+        .context("Could not convert to kmz")
 }
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -2,6 +2,9 @@ use assert_cmd::Command;
 
 #[test]
 fn test_bin() {
-    let mut cmd = Command::cargo_bin("strava2kmz").unwrap();
-    cmd.arg("tests/export.zip").assert().success();
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    cmd.current_dir("tests")
+        .arg("export.zip")
+        .assert()
+        .success();
 }
